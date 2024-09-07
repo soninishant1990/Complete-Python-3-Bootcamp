55685e5615150550170ef3c6a07258bccb43cf21
soninishant1990 <55386142+soninishant1990@users.noreply.github.com>
2024-09-07 16:02:58 -0400

    972024

--- 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ 06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,6 +1,8 @@
 from MyMainPackage.some_main_script import report_main
 from MyMainPackage.SubPackage import mysubscript
-
+# this note writtem by me
+## here MyMainPackage is main folder and SubPackage is sub folder and mysubscript is python script name 
+### sub_report is defined function inside the mysubscript script, similarly we can creat the our own package
 report_main()
 
 mysubscript.sub_report()
